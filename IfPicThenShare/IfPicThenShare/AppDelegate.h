1b40dfc59ddfeae0504b77de1223c1a76e2e92d1
Susong <yixin@11111111.local>
2014-07-01 11:00:07 +0800

    001
    001

---
+++ IfPicThenShare/IfPicThenShare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IfPicThenShare
+//
+//  Created by 宜信 on 14-7-1.
+//  Copyright (c) 2014年 creditease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IfPicThenShare/IfPicThenShare/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IfPicThenShare
+//
+//  Created by 宜信 on 14-7-1.
+//  Copyright (c) 2014年 creditease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
